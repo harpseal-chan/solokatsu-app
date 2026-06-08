7dfb56d8d6d30ff66384f943f9d9897e825f0e76
harpseal-chan <4062yoshi@gmail.com>
2026-06-08 13:38:13 +0900

    feat: 配色テーマの定義とヘッダーコンポーネント追加 #7

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div>
+      <header className="sticky top-0 z-50 w-full bg-sk-secondary backdrop-blur-md px-6 py-4 flex justify-between items-center">
+        <Link
+          href="/"
+          className="font-bold text-xl text-sk-text hover:text-sk-text/50 transition-colors duration-300"
+        >
+          ソロ活紹介アプリ
+        </Link>
+      </header>
+    </div>
+  );
+}
